a387feb751fa9fea3a79fdeda18be7c07f371d2c
Manuel Hahn <manuel.hahn@gmx.de>
2022-11-11 15:02:27 +0100

    Openshift location extractor mirroring & metrics (#4)
    * Added custom Prometheus exporter, exposing the number of extracted locations as well as the version of the location extractor (so far only some static version string).
    
    * - Added option to automatically deploy a generic second version of the location extractor
    - Added the possibility to additionally deploy a degraded version of the location extractor in order to compare results when using mirroring
    - Added monitoring for location extractor to expose number of extracted locations as well as its version
    - Added DestinationRule and VirtualService for location extractor to perform mirroring and 50/50 deployments using config options in helm values file
    - Added link to updated container image
    - Corrected variable name for workloadmonitoring flag in helm values file
    
    * cleanup.
    
    * cleanup.

--- components/location_extractor/src/server.py
+++ components/location_extractor/src/server.py
@@ -3,6 +3,14 @@ import spacy
 from geopy.geocoders import Nominatim
 import json
 import random
+from prometheus_client import Counter, Info, generate_latest
+import os
+import random
+
+# Define metrics for Prometheus export
+COUNTER_LOCATIONS_EXTRACTED = Counter('locations_extracted', 'Number of extracted locations')
+INFO_LOCATION_EXTRACTOR = Info('location_extractor', 'Information regarding the location extractor')
+INFO_LOCATION_EXTRACTOR.info({'version': str(os.getenv('LOC_EXT_VERSION'))})
 
 nlp_de = spacy.load('en_core_web_md')
 
@@ -29,12 +37,19 @@ def get_coords():
             print("Those entities were recognized as locations:" + str(locations), flush=True)
             geolocator = Nominatim(user_agent="my_app")
             for idx, location in enumerate(locations):
+                # Hack to simulate a worse-performing model in case of version v2
+                if str(os.getenv('LOC_EXT_VERSION')) == 'v2-worse-performance':
+                    if random.randint(0,1) == 0:
+                        continue
+
                 try:
                     loc = geolocator.geocode(location)
                     lat = loc.latitude
                     long = loc.longitude
                     locs_dict[idx+1] = {"extracted location": location, "generated address": loc.address, "latitude": lat - random.uniform(0.05, 2), "longitude": long + random.uniform(0.05, 2)}
                     print("found lat & long for this location: " + str(location), flush=True)
+                    # Increment metric for extracted locations
+                    COUNTER_LOCATIONS_EXTRACTED.inc(1)
                 except:
                     print("not found lat & long for this location:" + str(location), flush=True)
             jsonDict = json.dumps(locs_dict)
@@ -60,6 +75,11 @@ def get_coords():
                     }
 
     return jsonDict, 200, {'Content-Type': 'application/json; charset=utf-8'}
+
+# Expose metrics for Prometheus
+@app.route('/metrics')
+def metrics():
+    return generate_latest()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
